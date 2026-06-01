d1a69d231dee94678fb48cb80fbd7509b4645bca
Adam Bondarzewski <adam.bondarzewski@analyticpartners.com>
2026-06-01 13:17:45 +0000

    add alert open/close

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
 
   let items = ["New York", "San Francisco", "Tokyo", "London"];
 
   const handleSelectItem = (item: string) => console.log(item);
+  const handleAlertClose = () => setDisplayAlert(false)
+  const [displayAlert, setDisplayAlert] = useState(false)
 
-  const handleButtonClicked = (item: number) => console.log("Button state: ", item)
+  const handleButtonClicked = (item: number) => {
+    setDisplayAlert(true)
+    console.log("Button state: ", item)
+  }
 
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-    <Alert><h1>Big header </h1>
+  let alert = displayAlert &&
+    <Alert handleCloseClick={handleAlertClose}><h1>Big header </h1>
       <p> Some content</p>
     </Alert>
+
+  return <div>
+    {alert}
+    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
     <Button onButtonClicked={handleButtonClicked}> <h2> Button name</h2></Button>
   </div>
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  handleCloseClick: () => void
 }
 
-const Alert = ({ children }: Props) => {
+const Alert = ({ children, handleCloseClick}: Props) => {
   return (
     <div className="alert alert-primary alert-dismissable" role="alert">
       <button type="button" className="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hiden="true">&times;</span>
+        <span aria-hiden="true" onClick={handleCloseClick}>&times;</span>
       </button>
       {children}
     </div>
   )
 }
 
-export default Alert
+export default Alert 
